4dbe54e7ef75ec103ca66115f3bead49b0f41ba8
Nihad Keric <nihadkeric@gmail.com>
2021-09-27 00:14:27 +0200

    cleaning

--- apps/pr1/src/app/components/todo-list/todo-list.component.ts
+++ apps/pr1/src/app/components/todo-list/todo-list.component.ts
@@ -1,21 +1,11 @@
-import { Todo } from './../../../../../../libs/shared/data/src/lib/interfaces/todo.interface';
-import { TodoService } from './../../../../../../libs/shared/data/src/lib/todo.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng-mfe-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
-  todos: Todo[] = [];
-
-  constructor(private todoService: TodoService) { }
-
-  ngOnInit(): void {
-    this.todos = this.todoService.getData();
-    console.log(this.todos);
-    
-  }
+export class TodoListComponent {
+  todos: any[] = [];
 
 }
