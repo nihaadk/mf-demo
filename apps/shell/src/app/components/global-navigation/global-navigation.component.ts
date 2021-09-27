2417fbdb066cbbaaf16d7fa3b3f671bdd8ef4358
Nihad Keric <nihadkeric@gmail.com>
2021-09-27 19:53:46 +0200

    cleaning code in nav

--- apps/shell/src/app/components/global-navigation/global-navigation.component.ts
+++ apps/shell/src/app/components/global-navigation/global-navigation.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ng-mfe-global-navigation',
   templateUrl: './global-navigation.component.html',
   styleUrls: ['./global-navigation.component.scss']
 })
-export class GlobalNavigationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class GlobalNavigationComponent {}
